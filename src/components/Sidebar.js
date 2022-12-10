2ea20f2f79138b6a78e0014b49de3ac0c8c53b70
ahmedzafar0 <talhazafar0@gmail.com>
2022-12-11 01:51:10 +0500

    url

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -42,7 +42,7 @@ function Sidebar() {
     });
 
     function getRooms() {
-        fetch("https://phygibackend.herokuapp.com/rooms")
+        fetch("http://localhost:5000/rooms")
             .then((res) => res.json())
             .then((data) => setRooms(data));
     }
